030f8600a9da58cae83f125cab5033037c450b94
Dzmitry Arlou <dzmitry.arlou@apalon.com>
2021-09-08 12:59:05 +0300

    Initial commit

---
+++ SwiftAlgoPlayground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftAlgoPlayground
+//
+//  Created by Dzmitry Arlou on 9/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftAlgoPlayground/SwiftAlgoPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAlgoPlaygroundApp.swift
+//  SwiftAlgoPlayground
+//
+//  Created by Dzmitry Arlou on 9/8/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAlgoPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
